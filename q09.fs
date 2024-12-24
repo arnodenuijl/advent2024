22eed86e34a5103ee827daad3df97878f9b5807e
Arno den Uijl <aduijl@sogyo.nl>
2024-12-24 12:15:20 +0100

    9

--- q09.fs
+++ q09.fs
@@ -11,14 +11,14 @@ let input  =
     // testinput 
     File.ReadAllText "09.txt" 
 
-let checksum (line: Option<int> array) = 
+let checksum (line: Option<int> seq) = 
     line 
-    |> Array.mapi 
+    |> Seq.mapi 
         (fun i b -> 
             match b with 
             | Some b -> (bigint i) * (bigint b)
             | None -> bigint 0 )
-    |> Array.fold ((+)) (bigint 0)
+    |> Seq.fold ((+)) (bigint 0)
 
 let printLine xs =
     xs |> Seq.iteri (fun i x ->  
@@ -28,7 +28,6 @@ let printLine xs =
     )
 
 let q09a () =
-    Console.WriteLine "expand ...."
     let expanded = 
         input.ToCharArray()
         |> Array.mapi 
@@ -42,20 +41,28 @@ let q09a () =
             )
         |> Array.collect id
 
-    Console.WriteLine "find blocks and spaces ...."
-
-    let spaces, blocks = 
-        Array.fold 
-            (fun (i, spaces, blocks) item -> 
-                match item with 
-                | Some _ -> (i + 1, spaces, [yield! blocks; i])
-                | None -> (i + 1, [yield! spaces;i], blocks)
+    let blocks = 
+        expanded
+        |> Seq.mapi 
+            (fun i item -> 
+                if item.IsSome then 
+                    Seq.singleton i 
+                else Seq.empty
             )
-            (0, [], [])
-            expanded
-        |> fun (_, spaces, blocks) -> spaces, blocks
+        |> Seq.collect id
+        |> List.ofSeq
     
-    Console.WriteLine "Find changes ...."
+    let spaces = 
+        expanded
+        |> Seq.mapi 
+            (fun i item -> 
+                if not item.IsSome then 
+                    Seq.singleton i 
+                else Seq.empty
+            )
+        |> Seq.collect id
+        |> List.ofSeq
+    
     let altered = Array.copy expanded
     let changes = 
         let itemsToProcess = Math.Min(spaces.Length, blocks.Length) 
@@ -63,7 +70,6 @@ let q09a () =
             (List.take itemsToProcess spaces) 
             (List.take itemsToProcess (List.rev blocks)) 
     
-    Console.WriteLine "Apply changes ...."
     changes 
     |> List.iter 
         (fun (spaceIndex, blockIndex) -> 
@@ -72,14 +78,106 @@ let q09a () =
                 altered[blockIndex] <- None
         )
     
-    Console.WriteLine "Calculate checksum ...."
     let result = checksum altered
 
     Console.WriteLine $"q09a: {result}"  
     ()
 
+type Size = int32
+type FileId = int32
+type Block = Size * FileId
+
+type Spot = 
+| Block of Block
+| Spaces of Size
+
+let checksumSpotList spotList =
+    let reworked = 
+        spotList
+        |> List.fold (fun xs spot -> 
+                match spot with 
+                | Spaces s -> [ 
+                        yield! xs
+                        yield! List.replicate s None 
+                    ]
+                | Block (s,fileId) -> [
+                                yield! xs
+                                yield! List.replicate s (Some fileId)
+                            ] 
+           )
+           []
+    checksum reworked
 
 let q09b () = 
-    let result = ()
+    let numbers  = 
+        input.ToCharArray()
+        |> Array.map (Char.ToString >> int32)
+        |> Array.toList
+    
+    let blocksAndSpaces = [
+        let mutable start = 0
+        let mutable i = 0
+        for n in numbers do
+            yield
+                if Int32.IsEvenInteger i 
+                then Block (n, (i / 2))
+                else Spaces n
+            i <- i + 1
+            start <- start + n
+    ]
+    
+    let insertBlockInSpace space block =
+        match block, space with 
+        | Block (bSize, fileId), Spaces sSize when bSize <= sSize ->
+            [
+                Spaces 0
+                Block (bSize, fileId)
+                Spaces (sSize - bSize)
+
+            ]
+        | _ -> failwith $"kan niet een {block} in een {space} doen"
+
+    let processBlock (currentList: Spot list) ((blockSize, fileId) : Block) =
+        let indexOfBlock = List.findIndex ((=) (Spot.Block (blockSize, fileId))) currentList
+        
+        let indexOfFreeSpot = 
+            List.tryFindIndex
+                (fun ss ->
+                    match ss with 
+                    | Spaces s when s >= blockSize  -> true
+                    | _ -> false
+                ) 
+                currentList
+        
+        match indexOfFreeSpot with
+        | Some i when i < indexOfBlock -> 
+            let space = match currentList[i] with | Spaces s -> Spaces s ; | _ -> failwith "errrrrrrr."
+            let (blockSize, fileId) = match currentList[indexOfBlock] with | Block b -> b ; | _ -> failwith "errrrrrrr."
+            
+            let updatedList = 
+                currentList
+                |> List.removeAt indexOfBlock
+                |> List.insertAt indexOfBlock (Spaces blockSize)
+                |> List.removeAt i 
+                |> List.insertManyAt i (insertBlockInSpace space (currentList[indexOfBlock]))
+            updatedList
+        | _ -> 
+            currentList
+
+    let blocksReversed = 
+        blocksAndSpaces
+        |> List.collect (fun bs -> 
+            match bs with 
+            | Block b -> List.singleton b
+            | _ -> List.empty)
+        |> List.rev
+    
+    let finalList = 
+        blocksReversed 
+        |> List.fold (fun xs b -> 
+            processBlock xs b)
+            blocksAndSpaces
+
+    let result = checksumSpotList finalList
     Console.WriteLine $"q09b: {result}"
     ()
